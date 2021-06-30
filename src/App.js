58622f10c5b2ebf57e632f62ca66beb4a19827b6
joey <joeyainy@gmail.com>
2021-06-30 13:54:26 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import DrumPad from './DrumPad';
+import React from 'react';
 
 function App() {
+
+  let [displayText, setDisplayText] = React.useState("---");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="drum-machine">
+        <div id="display">{displayText}</div>
+        <DrumPad setDisplayText={setDisplayText} sampleName='kick' letter='Q'></DrumPad>
+        <DrumPad setDisplayText={setDisplayText} sampleName='snare' letter='W'></DrumPad>
+        <DrumPad setDisplayText={setDisplayText} sampleName='hihat' letter='E'></DrumPad>
+        <DrumPad setDisplayText={setDisplayText} sampleName='openhat' letter='A'></DrumPad>
+        <DrumPad setDisplayText={setDisplayText} sampleName='808' letter='S'></DrumPad>
+        <DrumPad setDisplayText={setDisplayText} sampleName='perc' letter='D'></DrumPad>
+        <DrumPad setDisplayText={setDisplayText} sampleName='perc' letter='Z'></DrumPad>
+        <DrumPad setDisplayText={setDisplayText} sampleName='clap' letter='X'></DrumPad>
+        <DrumPad setDisplayText={setDisplayText} sampleName='rim' letter='C'></DrumPad>
+      </div>
+      <p>coded by <a href="http://twitter.com/joeyiny">joey iny</a> 2021 😎</p>
     </div>
   );
 }
